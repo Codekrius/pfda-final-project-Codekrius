73b63c22495b2111c84e9b5a11c18beac2f07240
Conner Dunkelberger <CJDunkelberger01@gmail.com>
2026-05-11 16:13:04 -0500

    Commit 3 - Added variables for "flap_strength" and "gravity_strength" to the function "__init__" for easy balancing. - Changed the color of the player to blue "(0, 0, 255)" - Added floor and ceiling boudary visuals to prep for position check.

--- src/project.py
+++ src/project.py
@@ -7,15 +7,16 @@ class Flapping_avian():
         #GAME STATES
         self.spawned = False
         self.start = False
+        #GAME PHYSICS
+        self.flap_strength = -10
+        self.gravity_strength = 0.5
         #~~~~~~~~~~~~~~~~~~~
         #PLAYER SURFACE
         self.player = pygame.Rect(250, 300, 15, 15)
         self.size = size
-        self.color = pygame.Color(255, 0, 0)
+        self.color = pygame.Color(0, 0, 255)
         self.surface = self.update_surface()
-        #self.player_pos = (self.pos_x, self.pos_y)
         self.velocity = 0
-        self.acceleration = 0.05
         self.dead = False
         #PLAYER STATES
         self.flapping = False
@@ -70,14 +71,14 @@ class Flapping_avian():
             return self.flapping
     
     def gravity(self, dt):
-        self.velocity = self.velocity + self.acceleration
+        self.velocity = self.velocity + self.gravity_strength
 
     def update_pos(self, dt):
         self.gravity(self)
         self.player.y += self.velocity
         
     def flap(self, dt):
-        self.velocity = -5
+        self.velocity = self.flap_strength
 
 
 def main():
@@ -96,9 +97,11 @@ def main():
         flapping_avian.update(dt)
         black = pygame.Color(0, 0, 0)
         screen.fill(black)
+        pygame.draw.rect(screen, (255,0,0), pygame.Rect(0, 0, 800, 75))
+        pygame.draw.rect(screen, (255,0,0), pygame.Rect(0, 525, 800, 75))
         flapping_avian.draw(screen)
         pygame.display.flip()
-        dt=clock.tick(30)
+        dt=clock.tick(60)
     pygame.quit()
 
 if __name__ == "__main__":
